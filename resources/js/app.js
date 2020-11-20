a0d9731b38ae4c11b7687a0269d3b695ef440701
jayani65 <madarajayani@gmail.com>
2020-11-21 03:37:44 +0530

    second commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,22 @@
 require('./bootstrap');
 
+window.Vue = require('vue');
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
+
+const routes=[
+    
+    {path : '/dashboard', component:require('./components/dashboard.vue')},
+    {path :'/profile', component:require('./components/profile.vue')}
+
+]
+
+const router= new VueRouter({
+    routes,
+    mode : 'history'
+  });
+
+const app = new Vue ({
+    el : '#app',
+    router
+}); 
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,7 @@
 window._ = require('lodash');
 
+
+
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
@@ -23,6 +25,7 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
